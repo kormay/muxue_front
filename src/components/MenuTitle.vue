b98601393f5e4a03b8853e0b20ff8355ca5a8ff5
kormay <244277947@qq.com>
2019-05-12 14:42:59 +0800

    menu title

--- src/components/MenuTitle.vue
+++ src/components/MenuTitle.vue
@@ -30,35 +30,16 @@
       <!--<el-col :span="2"><div :span="2" class="kiss-icon"><img src="../assets/kiss/kiss17.jpeg" alt=""></div></el-col>-->
       <!--<el-col :span="2"><div :span="2" class="kiss-icon"><img src="../assets/kiss/kiss18.jpeg" alt=""></div></el-col>-->
     </el-row>
-
-    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-      <el-menu-item index="1">处理中心</el-menu-item>
-      <el-submenu index="2">
-        <template slot="title">我的工作台</template>
-        <el-menu-item index="2-1">选项1</el-menu-item>
-        <el-menu-item index="2-2">选项2</el-menu-item>
-        <el-menu-item index="2-3">选项3</el-menu-item>
-        <el-submenu index="2-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="2-4-1">选项1</el-menu-item>
-          <el-menu-item index="2-4-2">选项2</el-menu-item>
-          <el-menu-item index="2-4-3">选项3</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-menu-item index="3" disabled>消息中心</el-menu-item>
-      <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-    </el-menu>
   </div>
 </template>
 
 <style>
   .kiss-title {
     width: 100%;
-    /*height: 60px;*/
+    background: lightpink;
   }
   .kiss-icon {
     width: 100%;
-    /*height: 60px;*/
     border-radius: 50%;
   }
 </style>
