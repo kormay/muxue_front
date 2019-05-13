bb9caab18245cef4c2742b020d115c9f2da7ee5c
kormay <244277947@qq.com>
2019-05-13 23:09:08 +0800

    timeline slot

--- src/views/timeline/LoveLine.vue
+++ src/views/timeline/LoveLine.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="block">
-    <el-timeline style="margin-left: 30%;width: 40%;">
+    <el-timeline style="margin-left: 30%;width: 40%;margin-top: 5%">
       <el-timeline-item placement="top" v-for="(activity, index) in activities"
                         :key="index"
                         :icon="activity.icon"
@@ -8,7 +8,8 @@
                         :color="activity.color"
                         :size="activity.size"
                         :timestamp="activity.timestamp">
-        <el-card>
+        <div slot="dot"><img src="../../assets/kiss/kiss1.jpeg" style="width:30px;height:30px;border-radius: 15px;margin-left: -10px"></div>
+        <el-card shadow="hover" style="background-color: lightgray; margin-bottom: 40px">
           <h4>{{activity.content}}</h4>
           <p>{{activity.user}} 提交于 {{activity.timestamp}}</p>
         </el-card>
